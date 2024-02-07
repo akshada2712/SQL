6799cf62907636c4338ce35a0371d79d4905a40b
akshada2712 <62982666+akshada2712@users.noreply.github.com>
2024-02-06 23:14:11 -0500

    Time: 675 ms (80.01%), Space: 0B (100.00%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+/* Write your T-SQL query statement below */
+SELECT 
+DISTINCT author_id as id 
+from Views WHERE author_id = viewer_id 
+ORDER BY 
+id;
